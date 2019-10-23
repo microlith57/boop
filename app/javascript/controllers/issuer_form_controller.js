43d2c9b72b90f81767a733ee980a6bf991cdf094
microlith57 <microlith57@gmail.com>
2019-10-24 07:39:19 +1300

    feat(form): Fix issuer form bug; add config for email suffix
    Signed-off-by: microlith57 <microlith57@gmail.com>

--- app/javascript/controllers/issuer_form_controller.js
+++ app/javascript/controllers/issuer_form_controller.js
@@ -1,32 +1,31 @@
 import { Controller } from "stimulus";
 
-// BUG: [add text to name field] <tab> [add text to code field] <tab> doesn't complete 3rd field on either <tab>
 export default class extends Controller {
-  static targets = ['nameField', 'codeField', 'emailField']
-  static emailSuffix = '@spotswoodcollege.school.nz'
+  static targets = [
+    'nameField',
+    'codeField',
+    'emailField',
+    'defaultEmailSuffix'
+  ]
 
   connect() {
   }
 
-  // TODO: Configurable email suffix
+  // TODO: Refactor
   nameEdited() {
     const value = this.nameFieldTarget.value.toLowerCase().replace(/[^a-z]/g, '')
     this.codeFieldTarget.defaultValue = value
-    this.emailFieldTarget.defaultValue = (this.codeFieldTarget.value || value) + emailSuffix
+    this.emailFieldTarget.defaultValue = (this.codeFieldTarget.value || value) + this.emailSuffix()
   }
 
-  // TODO: Configurable email suffix
+  // TODO: Refactor
   codeEdited() {
     const value = this.codeFieldTarget.value.toLowerCase().replace(/[^a-z]/g, '')
-    this.emailFieldTarget.defaultValue = value + emailSuffix
+    this.emailFieldTarget.defaultValue = value + this.emailSuffix()
   }
-
-  setDefaults() {
-    if (this.codeFieldTarget.value == '') {
-      this.codeFieldTarget.value = this.codeFieldTarget.defaultValue
-    }
-    if (this.emailFieldTarget.value == '') {
-      this.emailFieldTarget.value = this.codeFieldTarget.defaultValue
-    }
+  
+  // Obtain the email suffix from the form.
+  emailSuffix() {
+    return this.defaultEmailSuffixTarget.value
   }
 }
