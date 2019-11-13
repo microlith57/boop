c773b15fc5cb94ec8ac9b47ba557f57c6ceca544
microlith57 <microlith57@gmail.com>
2019-11-13 19:30:55 +1300

    test: More tests
    Signed-off-by: microlith57 <microlith57@gmail.com>

--- spec/models/device_spec.rb
+++ spec/models/device_spec.rb
@@ -10,6 +10,13 @@ RSpec.describe Device do
       Timecop.return
     end
 
+    it 'can detect non-issued devices' do
+      device = build :device
+      expect(device.issued?).to be_falsy
+      expect(device.overdue?).to be_falsy
+      expect(device.days_overdue).to eq 0
+    end
+
     it 'can find overdue devices' do
       [
         p_time('4:25 pm NZST') - 1.day,
@@ -19,10 +26,12 @@ RSpec.describe Device do
         p_time('4:45 pm NZST') - 2.days
       ].each do |time|
         device = build :issued_device, issued_at: time
+        expect(device.issued?).to be_truthy
         expect(device.overdue?)
           .to be_truthy,
               "device issued at #{device.issued_at.to_s(:short)} " \
               "wasn't detected as overdue at #{DateTime.current.to_s(:short)}"
+        expect(device.days_overdue).to be >= 1
       end
     end
 
@@ -35,7 +44,9 @@ RSpec.describe Device do
         5.minutes.ago
       ].each do |time|
         device = build :issued_device, issued_at: time
+        expect(device.issued?).to be_truthy
         expect(device.overdue?).to be_falsey
+        expect(device.days_overdue).to eq 0
       end
     end
 
@@ -49,8 +60,37 @@ RSpec.describe Device do
         p_time('8:56 am NZST') - 5.days => 5
       }.each_pair do |time, days|
         device = build :issued_device, issued_at: time
+        expect(device.issued?).to be_truthy
         expect(device.days_overdue).to eq days
       end
     end
+  end
+
+  it 'can be issued' do
+    issuer = build :issuer
+    issuer.save!
+    device = build :device
+
+    device.issue issuer
+    device.save!
+    expect(device.issuer).to eq issuer
+    expect(issuer.devices).to include device
+  end
+
+  it 'can be returned' do
+    device = build :issued_device
+    device.save!
+    expect(device.issued?).to be_truthy
+
+    device.return
+    device.save!
+    expect(device.issued?).to be_falsy
+  end
+
+  it 'has a barcode' do
+    device = build :device
+    device.save!
+
+    expect(device.barcode).to be_present
   end
 end
