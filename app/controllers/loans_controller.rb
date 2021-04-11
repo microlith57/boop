969f954a9cb7e9afac013201a0f482bc6ed7e8f3
Microlith57 <microlith57@gmail.com>
2021-04-11 14:00:01 +1200

    perf(loans): remove redundant query includes
    Signed-off-by: Microlith57 <microlith57@gmail.com>

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -45,7 +45,7 @@ class LoansController < ApplicationController
 
     @q = Loan.where(device: @device).ransack query
     @q.sorts = 'created_at desc' if @q.sorts.empty?
-    result = @q.result.includes(:borrower, :device)
+    result = @q.result.includes(:borrower)
 
     respond_to do |format|
       format.html do
@@ -79,7 +79,7 @@ class LoansController < ApplicationController
 
     @q = Loan.where(borrower: @borrower).ransack query
     @q.sorts = 'created_at desc' if @q.sorts.empty?
-    result = @q.result.includes(:borrower, :device)
+    result = @q.result.includes(:device)
 
     respond_to do |format|
       format.html do
